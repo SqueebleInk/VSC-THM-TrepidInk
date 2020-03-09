182ed7137a6b8352c996cef68246babb8047b0aa
TessavWalstijn <twalstijn@hotmail.com>
2020-03-09 09:36:48 +0100

    added test samples

---
+++ src/test/samples/typescript.ts
@@ -0,0 +1,17 @@
+class Test
+{
+  /**
+   * TODO: Let it do stuff
+   */
+  public constructor ()
+  {
+    // NOTE: this is a constructor
+  }
+
+  /* FixMe a nice cold beer please */
+
+/*
+  NOTE that it not always works as we like...
+FixMe I mean look at this...
+*/
+}
